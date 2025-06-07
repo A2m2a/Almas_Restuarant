64d0ab82421d409f81b66acd97db312b3a0a3a7f
Asma Jafari <Asmajafary7@gmail.com>
2025-06-07 15:30:50 +0330

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace آخرین_الماس.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+
+        [NotMapped]
+        public string Password { get; set; }
+
+    }
+}
+
